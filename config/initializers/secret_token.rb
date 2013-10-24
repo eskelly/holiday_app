0ee98af77a514284ddc4721955d71dc36ff057cd
Eamon <eamon.skelly@sage.com>
2013-10-24 16:45:06 +0100

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HolidayApp::Application.config.secret_token = '77637e90522d6a7505734f7df369fdf6aabc8c49027c5acc5a95ce5b61a7ff3b529ce62a33da3ca25d87ed6cbfc59b65f3acba271708ac520f7c4e246d8de3ec'
